f204731d43aa9fa699090bb4967d0171ba36a37d
Alina <mikhail@MacBook-Pro-Mihail.local>
2026-06-26 14:53:52 +0300

    Inject serverless variables directly in php entry point to bypass Vercel env settings

--- api/index.php
+++ api/index.php
@@ -4,6 +4,25 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
+// Force serverless-friendly paths and configurations
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_EVENTS_CACHE=/tmp/events.php');
+putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+putenv('APP_SERVICES_CACHE=/tmp/services.php');
+putenv('VIEW_COMPILED_PATH=/tmp');
+putenv('LOG_CHANNEL=stderr');
+putenv('APP_DEBUG=true');
+
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
+$_ENV['APP_EVENTS_CACHE'] = '/tmp/events.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/routes.php';
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
+$_ENV['LOG_CHANNEL'] = 'stderr';
+$_ENV['APP_DEBUG'] = 'true';
+
 define('LARAVEL_START', microtime(true));
 
 try {
